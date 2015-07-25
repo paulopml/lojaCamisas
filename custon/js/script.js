82288d10342d395ba84a6d8f0c7afbfa156cce86
paulopml <paulo.marinho.leal@gmail.com>
2015-07-25 19:05:52 -0300

    primeira parte da home com a lista basica carregada poe jason

--- custon/js/script.js
+++ custon/js/script.js
@@ -1,3 +1,22 @@
 /**
  * Created by pauloml on 19/07/2015.
  */
+var app = angular.module('app1',[]);
+
+app.controller('controller1', function($scope){
+    $scope.produtos = meuProdutos;
+})
+
+var meuProdutos = [{
+    nome: 'Polo Lisa lilas',
+    preco: 29.00,
+    imagem : 'img/polo/3.jpg'
+},{
+    nome: 'Polo Listada',
+    preco: 29.00,
+    imagem : 'img/polo/6.jpg'
+},{
+    nome: 'Polo Lisa Cinza',
+    preco: 29.00,
+    imagem : 'img/polo/9.jpg'
+}];
